a2e4e5ea6916a16c2e7b4f2125cb702d415bcec5
Nanda Reynaldi <reynaldi@halojasa.id>
2020-09-25 00:47:18 +0700

    delete i18n initialization

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import i18n from '@app/i18n/handler'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
