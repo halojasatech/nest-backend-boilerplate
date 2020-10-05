06bd03d59968169fac07f6d2423800c7fc43d5d2
Restu Haqqi Muzakir <restuhaqza@gmail.com>
2020-10-05 15:42:42 +0700

    feat: add redis as key value database library

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInteceptor } from '@app/util/interceptors/response.interceptor';
 
+import { redis } from '@app/library'
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseInteceptor());
+  redis.createClient()
   await app.listen(3000);
 }
 bootstrap();
