e9afd5c0452b779f47ac0898b87d5212adeb5699
Nanda Reynaldi <reynaldi@halojasa.id>
2020-10-14 18:49:04 +0700

    fix: Fixing interceptor class name typo

--- src/app/util/interceptors/response.interceptor.ts
+++ src/app/util/interceptors/response.interceptor.ts
@@ -2,14 +2,13 @@ import {
   Injectable,
   NestInterceptor,
   ArgumentsHost,
-  HttpException,
   CallHandler,
 } from '@nestjs/common';
 import { map } from 'rxjs/operators';
 import { SuccessResponse } from '@app/exceptions/http-exception';
 
 @Injectable()
-export class ResponseInteceptor implements NestInterceptor {
+export default class ResponseInterceptor implements NestInterceptor {
   intercept(host: ArgumentsHost, next: CallHandler) {
     return next.handle().pipe(
       map((data: any) => {
